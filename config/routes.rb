f1351cc0e49b198550609d15ecb27c7943e096e2
beta_chelsea <beta.chelsea@gmail.com>
2023-03-21 20:18:47 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+  root to: redirect('/ideas')
   resources :ideas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
